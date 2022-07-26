29c1b85f6a1ed0baad088d1602d82d0ae5959f83
Amy Gardiner <amymgardiner@gmail.com>
2022-07-25 19:19:59 -0500

    created the post route to create or add candidates

--- server.js
+++ server.js
@@ -77,16 +77,14 @@ app.delete('/api/candidate/:id', (req, res) => {
 });
 
 // Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-//               VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-
-// db.query(sql, params, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
